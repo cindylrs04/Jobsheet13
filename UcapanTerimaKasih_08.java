8671330ab146e4864656d4983ff830c25b78c7f0
cindylrs04 <larasaticindy231@gmail.com>
2023-11-20 16:14:22 +0700

    first commit

---
+++ UcapanTerimaKasih_08.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class UcapanTerimaKasih_08 {
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;      
+    }
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like i could ask you anything.");
+        UcapanTambahan("maacii yaaww " + nama);
+    }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+    
+}
